fbbb25cc471bc60048b3f3a89282ed895149434e
Dan Shepherd <dan.x.shepherd@googlemail.com>
2019-03-10 16:14:24 +0000

    added image icons and app icon

---
+++ Frameworks/BLEControlFramework.framework/Headers/BLEControlFramework.h
@@ -0,0 +1,19 @@
+//
+//  BLEControlFramework.h
+//  BLEControlFramework
+//
+//  Created by Dan Shepherd on 23/07/2017.
+//  Copyright © 2017 Dan Shepherd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+//! Project version number for BLEControlFramework.
+FOUNDATION_EXPORT double BLEControlFrameworkVersionNumber;
+
+//! Project version string for BLEControlFramework.
+FOUNDATION_EXPORT const unsigned char BLEControlFrameworkVersionString[];
+
+
+
